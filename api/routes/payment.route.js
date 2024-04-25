eac3cd10dc87ae6744babf7cd6ac60d271bd2d61
kavipriya-sugumar <kavipriya.sugumar@digamend.com>
2024-04-25 09:50:53 +0530

    payment

--- api/routes/payment.route.js
+++ api/routes/payment.route.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import express from "express";
 // import { verifyToken,verifyUser, verifyTokenAndAdmin} from "../utils/verifyUser.js";
 // import { checkout } from "../controllers/payment.controller.js";
@@ -6,12 +5,3 @@
 
 // router.post("/checkout",verifyUser, checkout);
 // export default router;
-=======
-import express from "express";
-import { verifyToken,verifyUser, verifyTokenAndAdmin} from "../utils/verifyUser.js";
-import { checkout } from "../controllers/payment.controller.js";
-const router = express.Router();
-
-router.post("/checkout",verifyUser, checkout);
-export default router;
->>>>>>> 38a86b5f93c26ff8c37e60353da82fb9e9a88823
