6752b16beaf073939a493e0941115e54dc5af4ee
kavipriya-sugumar <kavipriya.sugumar@digamend.com>
2024-05-17 10:15:12 +0530

    changes updated

--- api/routes/asset.route.js
+++ api/routes/asset.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken, verifyTokenAndAdmin } from "../utils/verifyUser.js";
-import { createAsset, deleteAsset,  downloadAsset, getAssetById, updateAsset,  } from "../controllers/asset.controller.js";
+import {getAssetsByCategory, createAsset, deleteAsset,  downloadAsset, getAssetById, updateAsset,  } from "../controllers/asset.controller.js";
 import {  uploadFiles } from "../utils/s3UploadClient.js";
  
 const router = express.Router();
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/createAsset/:categoryId", uploadFiles, createAsset);
 router.get("/getAssets/:assetId",getAssetById);
 router.delete("/deleteasset/:assetId", deleteAsset);
-// router.get("/getByCategory/:id",assetsByCategory)
+router.get("/getByCategory/:id",getAssetsByCategory);
 // router.get("/getAllAssets", getAllAssets);
 router.put("/updateAsset/:categoryId/:assetId", updateAsset);
 router.get("/:assetId/files/:fileIndex/download",downloadAsset);
